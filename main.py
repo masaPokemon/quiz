ce4717c335c8a5e9ab44dfdfefc153c7478599eb
masaPokemon <masa.gongon2011@gmail.com>
2024-09-02 13:45:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import streamlit as st  
 from streamlit_qrcode_scanner import qrcode_scanner  
+import sqlite3 
 
-qr_code = qrcode_scanner(key='qrcode_scanner')  
+qrcodeButton = False
 
-if qr_code:  
-  st.write(qr_code) 
+if qrcodeButton:
+  qr_code = qrcode_scanner(key='qrcode_scanner')  
+
+  if qr_code:  
+    st.write(qr_code)
+
+# SQLiteに接続する
+conn = sqlite3.connect('/databse.db')
+# テーブルを作成する conn.execute('''
+ CREATE TABLE IF NOT EXISTS sleep_data ( 
+　　　id INTEGER PRIMARY KEY, 
+　　　date TEXT NOT NULL, 
+　　　sleep_time TEXT NOT NULL, 
+　　　wake_time TEXT NOT NULL５。 
+) 
+''')
