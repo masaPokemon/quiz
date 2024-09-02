377c228b9ebeca18d06b92d2a9e834d28600f464
masaPokemon <masa.gongon2011@gmail.com>
2024-09-02 13:18:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks import get_openai_callback
-
 from langchain.prompts import PromptTemplate
 from langchain.chains.summarize import load_summarize_chain
 from langchain.document_loaders import YoutubeLoader
