6835d74d84c180cf0e098e2958b5f8070c6f3f91
masaPokemon <masa.gongon2011@gmail.com>
2024-09-02 14:00:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
-import streamlit as st  
-from streamlit_qrcode_scanner import qrcode_scanner  
-import sqlite3 
-point2 = None
-def qrcodeButton():
-  qr_code = qrcode_scanner(key='qrcode_scanner')  
+import numpy as np
+import pandas as pd
+import sqlite3
+import seaborn as sns
 
-  if qr_code:  
-    st.write(qr_code)
-    return qr_code
+from pycaret.datasets import get_data
+#ボストンデータを取得
+df = get_data('boston')
 
-# SQLiteに接続する
-conn = sqlite3.connect('resource/database.db')
-# テーブルを作成する 
-conn.execute('''
-  CREATE TABLE IF NOT EXISTS Point ( 
-    id INTEGER PRIMARY KEY, 
-    userPoint TEXT NOT NULL, 
-  ) 
-''')
-if st.button("QRコードを表示"):
-  point = qrcodeButton()
-  conn.execute(f'''
-    INSERT INTO Point (userPoint)
-    VALUES ('{userPoint - point}')
-  ''')
-  conn.commit()
-st.title(point2)
+# データベース名とテーブル名
+db_name = 'datasets.db'
+table_name = 'tips'
+
+# SQLiteに書き込む
+with sqlite3.connect(db_name) as conn:
+    df.to_sql(table_name, conn)
