53381dfc6fcb60d65e680740764d2e68a4c8866d
masaPokemon <masa.gongon2011@gmail.com>
2024-09-02 12:04:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
-from pytube import YouTube, StreamQuery
+from io import BytesIO
+from pathlib import Path
+
 import streamlit as st
+from pytube import YouTube
 
-#最高の画質と音質を動画をダウンロードする
-fmt = "video"
-def download_file(stream, fmt):
-    """  """
-    title = stream.title
+st.set_page_config(page_title="Download Audio", page_icon="🎵", layout="centered", initial_sidebar_state="collapsed")
 
-    stream.download(filename=title)
-    
-text = st.text_input("URL：")
+@st.cache_data(show_spinner=False)
+def download_audio_to_buffer(url):
+    buffer = BytesIO()
+    youtube_video = YouTube(url)
+    audio = youtube_video.streams.get_audio_only()
+    default_filename = audio.default_filename
+    audio.stream_to_buffer(buffer)
+    return default_filename, buffer
 
-if text != "":#動画のURLを指定
-    download_file(text, fmt)
+def main():
+    st.title("Download Audio from Youtube")
+    url = st.text_input("Insert Youtube URL:")
+    if url:
+        with st.spinner("Downloading Audio Stream from Youtube..."):
+            default_filename, buffer = download_audio_to_buffer(url)
+        st.subheader("Title")
+        st.write(default_filename)
+        title_vid = Path(default_filename).with_suffix(".mp3").name
+        st.subheader("Listen to Audio")
+        st.audio(buffer, format='audio/mpeg')
+        st.subheader("Download Audio File")
+        st.download_button(
+            label="Download mp3",
+            data=buffer,
+            file_name=title_vid,
+            mime="audio/mpeg")
+
+if __name__ == "__main__":
+    main()
