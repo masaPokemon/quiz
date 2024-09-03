758c12f161fcf2796205a42434df268d48ef4f13
masaPokemon <masa.gongon2011@gmail.com>
2024-09-03 11:08:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,3 @@ import streamlit as st
 from pytube import YouTube, StreamQuery
 vid_url = st.text_input("youtubeのURL")
 yt = YouTube(vid_url).streams.first().download()
-if yt != "":
-    with open(yt,'rb') as fh:
-        st.download_button(
-            label = 'Download Video',
-            data = fh,
-            file_name = os.path.basename(file_url)
-        )
-    video_file = open("video.mp4", "rb")
-    video_bytes = video_file.read()
-
-    st.video(video_bytes)
