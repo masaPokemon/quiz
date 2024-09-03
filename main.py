ea1b1f29d8ff94520274a6a36756d6121786d532
masaPokemon <masa.gongon2011@gmail.com>
2024-09-03 11:18:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import urllib.request
 
 url = st.text_input("youtubeのURL")
 urllib.request.urlretrieve(url, './youtube.mp4')
+st.video
+
+video_file = open("youtube.mp4", "rb")
+video_bytes = video_file.read()
+
+st.video(video_bytes)
