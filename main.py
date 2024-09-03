def2a62dfe1d94da6c9a2aa4a4b0ececd4436e18
masaPokemon <masa.gongon2011@gmail.com>
2024-09-03 11:11:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import streamlit as st
-from pytube import YouTube, StreamQuery
-vid_url = st.text_input("youtubeのURL")
-yt = YouTube(vid_url).streams.first().download()
+import requests
+
+url=st.text_input("YoutubeのURL")
+filename='youtube.mp4'
+
+urlData = requests.get(url).content
+
+with open(filename ,mode='wb') as f: # wb でバイト型を書き込める
+  f.write(urlData)
