00e9fbd446808e7d488b7efd67ac96ee657a662f
masaPokemon <masa.gongon2011@gmail.com>
2024-09-03 11:44:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ url = st.text_input("youtubeのURL")
 
 ydl_opts = {
     'format':'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]',
-    'outtmpl':'youtube.%(ext)s',
+    'outtmpl':url + "mp4",
 }
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download([url])
 
-video_file = open("youtube.mp4", "rb")
+video_file = open(url + "mp4", "rb")
 video_bytes = video_file.read()
 
 st.video(video_bytes)
