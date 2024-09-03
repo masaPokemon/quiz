c5860a2b60f1cc705cc0c1dc999db875cec07b8b
masaPokemon <masa.gongon2011@gmail.com>
2024-09-03 11:16:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import streamlit as st
-import requests
+import urllib.request
 
-url=st.text_input("YoutubeのURL")
-filename='youtube.mp4'
-
-urlData = requests.get(url).content
-
-with open(filename,mode='wb') as f: # wb でバイト型を書き込める
-  st.html(f)
-  
+url = st.text_input("youtubeのURL")
+urllib.request.urlretrieve(url, './ex.html')
