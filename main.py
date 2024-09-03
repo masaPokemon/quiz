7067ea3861800895f163654a8322f23a82761f61
masaPokemon <masa.gongon2011@gmail.com>
2024-09-03 11:19:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import urllib.request
 
 url = st.text_input("youtubeのURL")
-urllib.request.urlretrieve(url, './youtube.mp4')
+urllib.request.urlretrieve(url, 'youtube.mp4')
 st.video
 
 video_file = open("youtube.mp4", "rb")
