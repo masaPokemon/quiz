222f5c112faa083f6c9907b4d44813cdaf5e3b75
masaPokemon <masa.gongon2011@gmail.com>
2024-09-03 11:54:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,4 @@
-from __future__ import unicode_literals
-import yt_dlp
-
 import streamlit as st
-import urllib.request
 
-url = st.text_input("youtubeのURL")
-
-ydl_opts = {
-}
-
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([url])
-
-
-import os
-
-dir_path = ""
-
-files = os.listdir(dir_path)
-
-video_file = open(files[0], "rb")
-video_bytes = video_file.read()
-
-st.video(video_bytes)
-
+VIDEO_URL = "https://www.youtube.com/watch?v=2wPf5XyUh1c"
+st.video(VIDEO_URL, subtitles="subtitles.vtt")
