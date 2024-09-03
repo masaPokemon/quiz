11332911c3c95be02477beedd83f18f86d444db2
masaPokemon <masa.gongon2011@gmail.com>
2024-09-03 11:13:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ filename='youtube.mp4'
 
 urlData = requests.get(url).content
 
-with open(filename ,mode='wb') as f: # wb でバイト型を書き込める
+with open(filename) as f: # wb でバイト型を書き込める
   st.html(urlData)
   
