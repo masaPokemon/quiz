2e4d814dbb5496adbaaff6b5dd1b2e2f7b83d846
masaPokemon <masa.gongon2011@gmail.com>
2024-09-03 11:32:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import streamlit as st
 import urllib.request
 
+from __future__ import unicode_literals
+import youtube_dl
+
 url = st.text_input("youtubeのURL")
-urllib.request.urlretrieve(url, "./youtube.mp4")
+
+ydl_opts = {}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
 
 video_file = open("youtube.mp4", "rb")
 video_bytes = video_file.read()
 
 st.video(video_bytes)
+
